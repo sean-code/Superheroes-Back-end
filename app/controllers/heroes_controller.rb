0cdb54704f2f32dc40dea0efad446c46ceb4671e
sean-code <john.nganga@student.moringaschool.com>
2022-12-03 14:50:12 +0300

    Apply custom rendering to notr display the created_at and updated_at keys

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,13 +1,13 @@
 class HeroesController < ApplicationController
     def index
         heroes = Hero.all
-        render json: heroes, status: :ok
+        render json: heroes, except:[:created_at, :updated_at], status: :ok
     end
 
     def show
         hero = Hero.find_by(id: params[:id])
         if hero
-            render json: hero, status: :ok
+            render json: hero, except:[:created_at, :updated_at], status: :ok
         else
             render json: {error: "Hero Not Found"}, status: :not_found
         end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,13 +1,13 @@
 class PowersController < ApplicationController
     def index
         powers = Power.all
-        render json: powers, status: :ok
+        render json: powers, except:[:created_at, :updated_at], status: :ok
     end
 
     def show
         power = Power.find_by(id: params[:id])
         if power
-            render json: power, status: :ok
+            render json: power, except:[:created_at, :updated_at], status: :ok
         else
             render json: {error: 'Power Not Found'}, status: :not_found
         end
