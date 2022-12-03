5438760e2e59551a80f3088b15ef099e483b18c5
sean-code <john.nganga@student.moringaschool.com>
2022-12-03 18:13:53 +0300

    Add powers attribute

--- app/serializers/hero_power_serializer.rb
+++ app/serializers/hero_power_serializer.rb
@@ -1,4 +1,4 @@
 class HeroPowerSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name
   belongs :hero
 end
--- app/serializers/hero_serializer.rb
+++ app/serializers/hero_serializer.rb
@@ -1,3 +1,3 @@
 class HeroSerializer < ActiveModel::Serializer
-  attributes :id, :name, :super_name
+  attributes :id, :name, :super_name, :powers
 end
