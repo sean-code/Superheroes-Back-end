52137aafc0cdd15fc5483b846ea3129ddd46fbe0
sean-code <john.nganga@student.moringaschool.com>
2022-12-03 14:03:39 +0300

    Add Validations

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,4 +1,11 @@
 class HeroPower < ApplicationRecord
     belongs_to :hero
     belongs_to :power
+
+    STRENGTH = ['Strong', 'Weak', 'Average']
+    validates :strength, inclusion:{
+        in: STRENGTH,
+        message: "Must be Either:
+        #{STRENGTH.join(',')}"
+    } 
 end
