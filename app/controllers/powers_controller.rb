c9330ff9d7435b063bc8a674cfdcb9f061bc7831
sean-code <john.nganga@student.moringaschool.com>
2022-12-03 14:38:40 +0300

    Add index and show methods

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,3 +1,13 @@
 class PowersController < ApplicationController
-    
+    def index
+        powers = Power.all
+        render json: powers, status: :ok
+    end
+
+    def show
+        power = Power.find_by(id: params[:id])
+        render json: power, status: :ok
+    end
 end
+
+
