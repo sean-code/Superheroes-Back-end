39711e8b6337ce638dc6844533c761ffbc59b60e
sean-code <john.nganga@student.moringaschool.com>
2022-12-03 14:29:08 +0300

    Add Show method

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,6 +1,12 @@
 class HeroesController < ApplicationController
     def index
-        heroes=Hero.all
-        render json: heroes, status: :created
+        heroes = Hero.all
+        render json: heroes, status: :ok
     end
+
+    def show
+        hero = Hero.find_by(id: params[:id])
+        render json: hero, status: :ok
+    end
+
 end
